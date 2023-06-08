994fb2a2db7e94dd979b5618c44d04d056ef9c74
phong.bnh <phong.bnh@globalfactories.com>
2023-06-08 19:42:56 +0700

    feat: add retry connect

--- PLAY.INVENTORY/src/Play.Inventory.Service/Program.cs
+++ PLAY.INVENTORY/src/Play.Inventory.Service/Program.cs
@@ -1,6 +1,8 @@
 using Play.Common.MongoDB;
 using Play.Inventory.Service.Clients;
 using Play.Inventory.Service.Entities;
+using Polly;
+using Polly.Timeout;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -15,6 +17,16 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddHttpClient<CatalogClient>(client => { client.BaseAddress = new Uri("https://localhost:5001"); })
+    .AddTransientHttpErrorPolicy(policyBuilder =>
+        policyBuilder.Or<TimeoutRejectedException>().WaitAndRetryAsync(3,
+            retryAttempt => TimeSpan.FromSeconds(Math.Pow(2, retryAttempt)), onRetry:
+            (result, span, retryAttempt) =>
+            {
+                var serviceProvider = builder.Services.BuildServiceProvider();
+                serviceProvider.GetService<ILogger<CatalogClient>>()!.LogWarning(result.Exception,
+                    "Delaying for {delay}ms, then making retry {retry}.", span.TotalMilliseconds,
+                    retryAttempt);
+            }))
     .AddPolicyHandler(Polly.Policy.TimeoutAsync<HttpResponseMessage>(1));
 
 var app = builder.Build();
